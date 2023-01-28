e1d5996dca5754fa0061b5118fccbe14c6c29e1c
Karamvir-Bains <kbains2000@gmail.com>
2023-01-28 14:16:56 -0700

    Add Appointment Show

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show.js";
 
 storiesOf("Button", module)
   .addParameters({
@@ -143,4 +144,5 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewers} onEdit={action("onEdit")} onDelete={action("onDelete")} />);
