83edf2309a66d01a7b7b4b3d698628be2197c24b
Karamvir-Bains <kbains2000@gmail.com>
2023-02-11 15:03:35 -0700

    bookInterview adds to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -32,6 +32,21 @@ export default function Application(props) {
 
   function bookInterview(id, interview) {
     console.log(id, interview);
+
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({
+      ...state,
+      appointments
+    });
   }
 
   const appointments = getAppointmentsForDay(state, state.day);
