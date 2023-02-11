418bdeb57f12bd72570eefd6b638e955b6148d4f
Karamvir-Bains <kbains2000@gmail.com>
2023-02-11 15:55:52 -0700

    Add Status to Appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,18 +4,23 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
+
+  const statusMessage = "Saving";
 
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
   function save(name, interviewer) {
+    transition(SAVING);
     const interview = {
       student: name,
       interviewer
@@ -38,6 +43,11 @@ export default function Appointment(props) {
         <Form
           onSave={save}
           interviewers={props.interviewers}
+        />
+      )}
+      {mode === SAVING && (
+        <Status
+          message={statusMessage}
         />
       )}
     </article>
